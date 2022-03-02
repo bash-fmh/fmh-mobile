1bc3ddee5f936aac9a1e665caa7c780c01d77b1e
yeanloong96 <yeanloong@foodmarkethub.com>
2022-03-02 11:41:55 +0800

    move icon directory

--- lib/core/constant/constant_asset.dart
+++ lib/core/constant/constant_asset.dart
@@ -3,8 +3,10 @@ class ConstantAsset {
 
   static const String _imagePath = 'asset/image/';
 
+  static const String _iconPath = 'asset/icon/';
+
   static const String block = '${_imagePath}block.png';
   static const String search = '${_imagePath}search.png';
   static const String cart = '${_imagePath}cart.png';
-  static const String splashIcon = '${_imagePath}splashicon.png';
+  static const String splashIcon = '${_iconPath}splashicon.png';
 }
