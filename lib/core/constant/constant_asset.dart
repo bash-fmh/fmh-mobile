b7653822f2e37c99c2410bd0b30b80b8d9b1145b
Galang Lintar <galanglintar6@gmail.com>
2022-03-04 15:39:12 +0700

    Update localization and constant asset

--- lib/core/constant/constant_asset.dart
+++ lib/core/constant/constant_asset.dart
@@ -7,4 +7,12 @@ class ConstantAsset {
   static const String search = '${_imagePath}search.png';
   static const String cart = '${_imagePath}cart.png';
   static const String splashIcon = '${_imagePath}splashicon.png';
+  static const String fmhLoginLogo = '${_imagePath}fmh_login_logo.png';
+  static const String fmhLoginImage = '${_imagePath}fmh_login_image.png';
+  static const String chevronDown = '${_imagePath}chevron_down.png';
+  static const String flagHK = '${_imagePath}flag_hk.png';
+  static const String flagID = '${_imagePath}flag_id.png';
+  static const String flagMY = '${_imagePath}flag_my.png';
+  static const String flagSG = '${_imagePath}flag_sg.png';
+  static const String flagTW = '${_imagePath}flag_tw.png';
 }
--- lib/core/service/localization/get_localization.dart
+++ lib/core/service/localization/get_localization.dart
@@ -6,6 +6,20 @@ class GetLocalization {
   String get search => localizationService.text('search');
   String get profile => localizationService.text('profile');
   String get noInternet => localizationService.text('no_internet');
+  String get appQuote => localizationService.text('app_quote');
+  String get kfc => localizationService.text('kfc');
+  String get ph => localizationService.text('ph');
+  String get username => localizationService.text('username');
+  String get password => localizationService.text('password');
+  String get mobileNumber => localizationService.text('mobile_number');
+  String get mobileNumberHint => localizationService.text('mobile_number_hint');
+  String get continueText => localizationService.text('continue');
+  String get login => localizationService.text('login');
+  String get termsOfUseStatement =>
+      localizationService.text('terms_of_use_statement');
+  String get termsOfUse => localizationService.text('terms_of_use');
+  String get and => localizationService.text('and');
+  String get privacyPolicy => localizationService.text('privacy_policy');
 
   String testArg1({required String parm1, required String parm2}) =>
       '$parm1 ${localizationService.text('no_internet')} $parm2';
